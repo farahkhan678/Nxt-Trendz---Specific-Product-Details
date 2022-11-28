f9f9b076f3aa18dbf97a2b535e7ee3360c037c67
farahkhan678 <441farahkhan@gmail.com>
2022-11-28 11:25:22 +0000

    first commit

--- src/components/SimilarProductItem/index.js
+++ src/components/SimilarProductItem/index.js
@@ -15,7 +15,7 @@ const SimilarProductItem = props => {
       <p className="similar-product-title">{title}</p>
       <p className="similar-products-brand">by {brand}</p>
       <div className="similar-product-price-rating-container">
-        <p className="similar-product-price">Rsll {price}/-</p>
+        <p className="similar-product-price">Rs {price}/-</p>
         <div className="similar-product-rating-container">
           <p className="similar-product-rating">{rating}</p>
           <img
